ffe496832371dffc86782ae2d09135cf7f232bd4
mirshaduk <119918613+mirshaduk@users.noreply.github.com>
2024-07-27 09:40:45 +0530

    Add files via upload

---
+++ oops2.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+// we achiee this using somthing  called a class
+
+class student
+{
+    public:
+
+     string name;
+    int mids;
+    float ends;
+    bool pre_abs;
+};
+
+class vhicle
+{
+
+    public:
+
+    string car_name;
+    string tyre_name;
+    int number_of_tyres;
+    int number_of_seats;
+
+};
+int main()
+{
+    // there are a class of 30 students and each student has the following data associated with him :
+
+   
+    student a,b,c;
+    int d, e, f;
+
+    vhicle hyundai, suzuki, ferrari;
+    //  a b c over here are objects of a perticular class
+}
